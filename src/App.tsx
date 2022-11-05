75f92ff18315a38669b007fd3dbaca34c7843c97
Harangus Robert <potatogolden76@gmail.com>
2022-11-05 20:05:56 +0200

    adwadad

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,19 @@ import styles from "./styles/app.module.scss"
 import Footer from './components/footer';
 import Headbar from './components/Headbar';
 
+import { CircularProgress } from '@mui/material';
+
 function App() {
   return (
     <>
-    <Headbar />
-      <section className={styles.stats}></section>
-      <section className={styles.menu}></section>
+      <Headbar />
+      <section className={styles.stats}>
+        <CircularProgress className={styles.xp} size={120} thickness={5} color={"warning"} value={90} variant={"determinate"} />
+        <p className={styles.sideText}>You have discovered 21 places so far</p>
+      </section>
+      <section className={styles.menu}>
+        
+      </section>
       <Footer />
     </>
 
